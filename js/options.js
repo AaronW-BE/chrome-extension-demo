1a34b459b7d2ca98f8f1c373d162f087521a13ad
AaronW <zmtzwb@gmail.com>
2020-11-27 00:07:46 +0800

    引入vue

--- js/options.js
+++ js/options.js
@@ -1,14 +1,22 @@
-const fileInput = document.getElementById('file');
-
-fileInput.addEventListener('change', fileChanged);
-
-function fileChanged(e) {
-    let files = e.target.files, reader = new FileReader();
-    reader.onload = _imp;
-    reader.readAsText(files[0]);
-}
-
-function _imp() {
-    let _myImportedData = JSON.parse(this.result);
-    console.log(_myImportedData)
-}
+let app = new Vue({
+    el: '#app',
+    data: {},
+    mounted() {
+        this.$refs.file.addEventListener('change', this.fileChanged);
+    },
+    methods: {
+        fileChanged(e) {
+            let files = e.target.files, reader = new FileReader();
+            reader.onload = this._imp;
+            reader.readAsText(files[0]);
+        },
+        _imp() {
+            try {
+                let _myImportedData = JSON.parse(this.result);
+                console.log(_myImportedData)
+            } catch (e) {
+                alert('解析错误')
+            }
+        }
+    }
+})
